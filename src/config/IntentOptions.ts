ac1d8db0af396329b78717ddecc551f64463be56
Tsuchinoko69 <anupatel69@gmail.com>
2022-11-16 19:29:15 +0800

    Changes to be committed: modified:   .gitignore modified:   package-lock.json modified:   package.json new file:   src/config/IntentOptions.ts modified:   src/index.ts

---
+++ src/config/IntentOptions.ts
@@ -0,0 +1,3 @@
+import { IntentsString } from "discord.js";
+export const IntentOptions: IntentsString[] = ["GUILDS"];
+//the above tells Discord that the bot will recieve guild events
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log
+import { Client } from "discord.js";
+import { IntentOptions } from "./config/IntentOptions";
+
+
+(async () => {
+    //instantiate a new client
+    const BOT = new Client ({intents: IntentOptions});
+    // connect to Discord Gateway with .login() method
+    await BOT.login(process.env.BOT_Token);
+})();
