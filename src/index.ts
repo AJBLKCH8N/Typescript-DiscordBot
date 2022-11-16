febe0e2c30aff22365304968361f01b305303322
Tsuchinoko69 <anupatel69@gmail.com>
2022-11-17 07:01:50 +0800

    enter on master

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import { Client } from "discord.js";
+import { Client, Interaction } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
+import { connectDatabase } from "./database/connectDatabase";
+import { validateEnv } from "./utils/validateEnv";
+import { onInteraction } from "./events/onInteraction";
 
 
 (async () => {
+    //if (!validateEnv()) return;
     //instantiate a new client
     const BOT = new Client ({intents: IntentOptions});
     // connect to Discord Gateway with .login() method
-    await BOT.login(process.env.BOT_Token);
+    BOT.on("ready", () => console.log("Connected to Discord"));
+    await connectDatabase();
+    await BOT.login(process.env.BOT_Token as string);
+    BOT.on(
+        "interactionCreate",
+        async (interaction) => await onInteraction(interaction)
+    );
+
 })();
